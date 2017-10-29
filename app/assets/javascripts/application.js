d949c94d92dc376b49dc34e1fc9a36f222196251
鳥居健太郎 <kentaro@MacBook-Air.local>
2017-10-29 12:48:55 +0900

    exam2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,17 +17,6 @@
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-  function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $('#img_prev').attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  $("#post_post_img").change(function() {
-    readURL(this);
-  });
+jQuery( function($) {
+    $('a').tooltip();
 });
