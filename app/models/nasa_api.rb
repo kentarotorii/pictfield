2a906fffbda1b343002d1aa6573b3a1aa13a8c55
鳥居健太郎 <kentaro@MacBook-Air.local>
2017-10-29 15:01:12 +0900

    exam2

--- app/models/nasa_api.rb
+++ app/models/nasa_api.rb
@@ -1,4 +1,4 @@
-class NasaApi < ActiveRecord::Base
+class NasaApi
   def self.get_api_info
     nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
     request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024134648) do
+ActiveRecord::Schema.define(version: 20171029053759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171024134648) do
     t.string "provider", default: "", null: false
     t.string "image_url"
     t.string "avatar"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
